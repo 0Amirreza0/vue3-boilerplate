a877380d089f0ef99806fdd11508b058db3d05ff
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2023-07-28 11:45:17 +0330

    chore: remove extra files

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,3 @@
-const ROUTES = [
-  {
-    path: '/',
-    redirect: { name: 'note-list' },
-  },
-  {
-    name: 'note-list',
-    path: '/note-list',
-    component: () => import('@/views/note-list.vue'),
-  },
-];
+const ROUTES = [];
 
 export default ROUTES;
--- src/views/note-list.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>THIS IS NOTE LIST VIEW</h1>
-</template>
