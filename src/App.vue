3f5b086a0339817a351a0208ab57d2bb031b878d
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2023-08-24 20:00:28 +0330

    feat: implement a basic component as an example

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,51 @@
   <layout-view>
     <router-view />
   </layout-view>
+  <linear-indicator
+    v-if="indicatorPercentage"
+    class="indicator"
+    :percentage="indicatorPercentage"
+  />
 </template>
 
 <script setup>
+import { onUnmounted, ref } from 'vue';
+
+import eventBus from '@/services/event-bus';
+
 import LayoutView from '@/layout/layout-view.vue';
+import LinearIndicator from '@/components/indicator/linear-indicator.vue';
+
+const indicatorPercentage = ref(0);
+
+const updateIndicatorPercentage = (percentage) => {
+  indicatorPercentage.value = percentage;
+};
+const beforeEachSubscription = eventBus.subscribeOn('router:beforeEach', () =>
+  updateIndicatorPercentage(30)
+);
+
+const afterEachSubscription = eventBus.subscribeOn('router:afterEach', () => {
+  updateIndicatorPercentage(100);
+  setTimeout(() => updateIndicatorPercentage(0), 300);
+});
+
+const clearSubscriptions = () => {
+  beforeEachSubscription.unsubscribe();
+  afterEachSubscription.unsubscribe();
+};
+onUnmounted(clearSubscriptions);
 </script>
 
 <style lang="scss">
 // FIXME: remove this style tag
+</style>
+
+<style lang="scss" scoped>
+.indicator {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+}
 </style>
---
+++ src/components/indicator/linear-indicator.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="track">
+    <div class="track__indicator"></div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  percentage: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const indicatorPercentage = computed(() => `${props.percentage}%`);
+</script>
+
+<style lang="scss" scoped>
+.track {
+  background-color: var(--linear-indicator-track-color, transparent);
+  border-radius: var(--linear-indicator-radius, 0.25rem);
+  width: 100%;
+
+  &__indicator {
+    height: var(--linear-indicator-stroke, 0.25rem);
+    width: v-bind(indicatorPercentage);
+    border-radius: var(--linear-indicator-radius, 0.25rem);
+    background: var(--linear-indicator-color, #00baba);
+    transition: var(--linear-indicator-transition, width ease-out 0.25s);
+  }
+}
+</style>
